51e65a4a0e36c77518b10fd8391146f4c487c469
m-nwajha <m.nawjha@gmail.com>
2026-08-02 17:03:15 +0300

    fix ProjectsHero

--- src/components/views/Projects/ProjectsHeroSect.tsx
+++ src/components/views/Projects/ProjectsHeroSect.tsx
@@ -3,9 +3,7 @@
 import { useHeroReveal } from "@/hooks/useHeroReveal";
 import { useWebflowReinit } from "@/hooks/useWebflowReinit";
 
-const HERO_ELEMENT_IDS = [
-  "31fc61d2-2492-0255-6d28-e6250474cf67",
-];
+const HERO_ELEMENT_IDS = ["31fc61d2-2492-0255-6d28-e6250474cf67"];
 
 const ProjectsHeroSect = () => {
   useWebflowReinit(["ix2"]);
@@ -19,11 +17,11 @@ const ProjectsHeroSect = () => {
           className="ul_service_hero_banner_title_box"
         >
           <p className="ul_short_des ul_blog_hero_banner_des">
-            Ein Einblick in unsere Arbeit - Projekte, die zeigen, wie wir
-            Ideen mit Wirkung
+            Projekte, die zeigen, wie Ideen zu starken Marken werden.
           </p>
           <h1 className="ul_inner_hero_banner_title ul_service_details_hero_banner_title">
-            Die Arbeit<span className="ul_blog_hero_banner_title_span">spricht</span>
+            UNSERE{" "}
+            <span className="ul_blog_hero_banner_title_span">ARBEIT</span>
           </h1>
         </div>
       </div>
